be1d6512d485820484b31c5a0fa6ce81599eb27c
seaho5599 <seaho5599@gmail.com>
2022-09-05 18:00:25 +0900

    git stx,마시그래이,노엘로 page 연결

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-window.onload = function(){
+window.onload = function () {
   AOS.init();
+
 }
+$(document).ready(function (){
+  // 스크롤 적용
+  setTimeout(function () {
+    $(".header").niceScroll({
+      cursorwidth: "8px",
+      cursoropacitymax: 0.7,
+    });
+  }, 0);
+
+  $(window).resize(function () {
+    $(".header").niceScroll({
+      cursorwidth: "8px",
+      cursoropacitymax: 0.7,
+    });
+  });
+
+
+})
