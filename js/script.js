f4999f170ce152cb1c18b74177dfb07d5e9f6c66
seaho5599 <seaho5599@gmail.com>
2022-09-08 18:07:24 +0900

    구성 구상중

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ window.onload = function () {
 
 }
 $(document).ready(function (){
+  let goTop = $('.gotop')
+  $(window).scroll(function(){
+    let temp = $(window).scrollTop();
+    console.log(temp)
+    if(temp < 400){
+      goTop.stop().fadeOut(200)
+    }else{
+      goTop.stop().fadeIn(200)
+    }
+    goTop.click(function(){
+      $('html').stop().animate({
+        scrollTop: 0
+      }, 1000);
+    })
+  })
+
   // 스크롤 적용
   setTimeout(function () {
     $(".header").niceScroll({
@@ -21,4 +37,27 @@ $(document).ready(function (){
     delay: 10,
     time: 1000
 });
+  let all = $('.all')
+  let personal = $('.personal')
+  let study = $('.study')
+  let vue = $('.vue')
+
+  all.click(function(){
+    $('.protfoio-clone').stop().fadeIn(400);
+  })
+  personal.click(function(){
+    $('.p-personal').stop().fadeIn(400);
+    $('.p-study').stop().fadeOut(400);
+    $('.p-vue').stop().fadeOut(400);
+  })
+  study.click(function(){
+    $('.p-study').stop().fadeIn(400);
+    $('.p-personal').stop().fadeOut(400);
+    $('.p-vue').stop().fadeOut(400);
+  })
+  vue.click(function(){
+    $('.p-vue').stop().fadeIn(400);
+    $('.p-study').stop().fadeOut(400);
+    $('.p-personal').stop().fadeOut(400);
+  })
 })
